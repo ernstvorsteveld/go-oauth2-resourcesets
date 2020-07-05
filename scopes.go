e7d46e48f41d3de28cd3e3df5e6e14092f0f57fc
Ernst Vorsteveld <ernst.vorsteveld@gmail.com>
2020-07-05 16:17:37 +0200

    moved use cases

--- scopes.go
+++ scopes.go
@@ -56,17 +56,6 @@ type Scope struct {
 	IconURI     *URL   `json:"icon_uri"`
 }
 
-// UseCases is the set of functions we can do with a Scope:
-// Get: retrieve the scope by its URL, throws error when not available,
-// Create: create for the URL a new Scope. If the URL already has a scope, it is overwritten,
-// Delete: deletes the scope that belongs to the URL.
-type UseCases interface {
-	Get(name URL) (*Scope, error)
-	Create(name URL, scope Scope)
-	Upsert(name URL, scope Scope)
-	Delete(name URL)
-}
-
 // Gateway is the use case
 type Gateway struct {
 	gw GatewayUseCases
--- scopes_use_cases.go
+++ scopes_use_cases.go
@@ -1,1 +1,12 @@
 package scopes
+
+// UseCases is the set of functions we can do with a Scope:
+// Get: retrieve the scope by its URL, throws error when not available,
+// Create: create for the URL a new Scope. If the URL already has a scope, it is overwritten,
+// Delete: deletes the scope that belongs to the URL.
+type UseCases interface {
+	Get(name URL) (*Scope, error)
+	Create(name URL, scope Scope)
+	Upsert(name URL, scope Scope)
+	Delete(name URL)
+}
