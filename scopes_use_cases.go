ad98dde00dbe9e82e6d8e96c66e84da837df3a99
Ernst Vorsteveld <ernst.vorsteveld@gmail.com>
2020-07-06 14:27:28 +0200

    fixed linter issue in comments

--- scopes_use_cases.go
+++ scopes_use_cases.go
@@ -1,6 +1,6 @@
 package scopes
 
-// UseCases is the set of functions we can do with a Scope:
+// ScopeUseCases is the set of functions we can do with a Scope:
 // Get: retrieve the scope by its URL, throws error when not available,
 // Create: create for the URL a new Scope. If the URL already has a scope, it is overwritten,
 // Delete: deletes the scope that belongs to the URL.
